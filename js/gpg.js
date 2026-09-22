e592ad616448c9bc0f9adaf28f700a1e362d3bc6
emretosn <emre7osun@gmail.com>
2026-09-22 15:07:55 +0200

    feat: hower to reveal gpg

--- js/gpg.js
+++ js/gpg.js
@@ -1,19 +1,12 @@
 const emailLink = document.getElementById("email-link");
 const gpgLine = document.getElementById("gpg-line");
-let pinned = false;
 
 if (emailLink && gpgLine) {
-  emailLink.addEventListener("mouseenter", () => {
-    gpgLine.classList.add("show");
-  });
-
-  emailLink.addEventListener("mouseleave", () => {
-    if (!pinned) {
-      gpgLine.classList.remove("show");
-    }
-  });
-
-  emailLink.addEventListener("click", () => {
-    pinned = true;
-  });
+  emailLink.addEventListener(
+    "mouseenter",
+    () => {
+      gpgLine.classList.add("show");
+    },
+    { once: true }
+  );
 }
